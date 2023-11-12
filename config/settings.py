c327db3d0a368235414ab9f2486d42795f5fa223
ABEL <abelovearn@gmail.com>
2023-11-13 00:07:23 +0300

    fix mistakes 3

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': os.getenv('DATABASES_NAME'),
         'USER': os.getenv('DATABASES_USER'),
         'PASSWORD': os.getenv('DATABASES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
     }
 }
 
